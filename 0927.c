b5c00f30fe4cd27e3d410dcdf49a4649ba210fa8
Bailu2004 <115567869+Bailu2004@users.noreply.github.com>
2022-10-12 21:14:30 +0800

    Add files via upload

---
+++ 0927.c
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main()
+{
+	double a = 2.34E07;
+	printf("%le", a);
+}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//int a = 2147483647;
+//	printf("整数上溢是：\n");
+//	printf("%d %d %d\n", a , a+1 , a+2);
+//
+//	float b = 3.402823E38;
+//	printf("浮点数上溢是：\n");
+//	printf("%f %f\n", b, b * 100);
+//
+//	float c = (3.4E-38) / (100.0E100);
+//	printf("浮点数下溢是：\n");
+//	printf("%lf", c);
+//
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	printf("请输入一个ASCII码值：");
+//	scanf("%d", &a);
+//	printf("ASCII码值为%d的字符是%c", a, a);
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("\a");
+//	printf("Startled by the sudden sound, Sally shouted,\n");
+//	printf( "By the Great Pumpkin, what was that!");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//void main()
+//{
+//	float a;
+//	printf("Enter a floating-point value:");
+//	scanf("%f", &a);
+//	printf("fixed-point notation:%f\n", a);
+//	printf("exponential notation:%e\n", a);
+//	printf("p notation:%a\n", a);
+//
+//}
+
+//#include<stdio.h>
+//void main()
+//{
+//	int age;
+//	double second;
+//	printf("Enter your age:");
+//	scanf("%d", &age);
+//	second = age * 3.156E7;
+//	printf("%d years include %lf seconds ", age, second);
+//}
+
+//#include<stdio.h>
+//void main()
+//{
+//	float number,m;
+//	printf("请输入水的夸脱数：");
+//	scanf("%f", &number);
+//	m = (number * 950) / (3.0E-23);
+//	printf("水分子的数量是：%f", m);
+
+//}
+
+
+//#include<stdio.h>
+//void main()
+//{
+//	float inch,cm;
+//	printf("Enter your height(inch):");
+//	scanf("%f", &inch);
+//	cm = inch * 2.54;
+//	printf("your height is %f cm", cm);
+//}
+
+//#include<stdio.h>
+//void main()
+//{
+//	float cup;
+//	float pint;
+//	float ounce;
+//	float bigspoon;
+//	float teaspoon;
+//	printf("请输入杯数：");
+//	scanf("%f", &cup);
+//	pint = cup / 2;
+//	ounce = cup * 8;
+//	bigspoon = ounce * 2;
+//	teaspoon = bigspoon / 3;
+//	printf("Here are some ways to show %f cups\n", cup);
+//	printf("Pint: %f\n", pint);
+//	printf("Ounce: %f\n", ounce);
+//	printf("Bigspoon: %f\n", bigspoon);
+//	printf("Teaspoon: %f\n", teaspoon);
+//}
+
+
